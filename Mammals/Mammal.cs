8acc0c72f5537d007b1a5867a08bd4b33cbd8fd5
Ivaylo Donchev <idonchev@hotmail.com>
2017-04-10 08:29:19 +0300

    kkk

---
+++ Mammals/Mammal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mammals
+{
+    public class Mammal
+    {
+        public void Breathe()
+        {
+            //..................
+        }
+        public void SuckleYoung()
+        {
+            //..................
+        }
+    }
+    public class Horse : Mammal
+    {
+        public void Trot()  // Тръс, бърз ход
+        {
+            //..................
+        }
+    }
+    public sealed class Whale : Mammal
+    {
+        public void Swim()
+        {
+            //..................
+        }
+    }
+}
--- Mammals/Program.cs
+++ Mammals/Program.cs
@@ -10,6 +10,28 @@ namespace Mammals
     {
         static void Main(string[] args)
         {
+            Horse horse = new Horse();
+            horse.Trot();
+            horse.Breathe();
+            horse.SuckleYoung();
+
+            Whale whale = new Whale();
+            whale.Swim();
+            whale.Breathe();
+            whale.SuckleYoung();
+        
+            var horseMethods = typeof(Horse).GetMethods(System.Reflection.BindingFlags.Public | 
+                                                        System.Reflection.BindingFlags.Instance);
+            foreach(var method in horseMethods)
+            {
+                var parameters = method.GetParameters();
+                Console.Write($"{method.ReturnType.Name} {method.Name}(");
+                foreach (var parameter in parameters)
+                {
+                    Console.Write($"{parameter.ParameterType.Name} {parameter.Name} ");
+                }
+                Console.WriteLine(")");
+            }
         }
     }
 }
