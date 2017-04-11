4a514c45787a04369be3fe7e72f65a9aeb490b8b
Ivaylo Donchev <idonchev@hotmail.com>
2017-04-11 19:00:30 +0300

    dfg

---
+++ NegInt32/NegInt32/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegInt32
+{
+    public static class Util
+    {
+        public static int Negate(this int x)
+        {
+            return -x;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine(i.Negate());
+            Console.WriteLine(Util.Negate(i));
+        }
+    }
+}
