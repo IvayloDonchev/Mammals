8b3361b0a47fb6f5196f6b5af6034729fc33046d
Ivaylo Donchev <idonchev@hotmail.com>
2017-04-12 08:38:54 +0300

    sdgf

---
+++ ExtensionMethod/ExtensionMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void doWork(int x)
+        {
+            for (int i = 2; i <= 16; i++)
+            {
+                Console.WriteLine($"{x} in base {i} is {x.ConvertToBase(i)}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number to convert:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                doWork(x);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExtensionMethod/ExtensionMethod/Util.cs
@@ -0,0 +1,20 @@
+﻿namespace ExtensionMethod
+{
+    public static class Util
+    {
+        public static string ConvertToBase(this int i, int baseToConvertTo)
+        {
+            string result = "";
+            do
+            {
+                int nextDigit = i % baseToConvertTo;
+                i /= baseToConvertTo;
+                if(nextDigit<10)
+                    result = nextDigit.ToString() + result;
+                else
+                    result = (char)('A'+ (nextDigit-10)) + result;
+            } while (i != 0);
+            return result;
+        }
+    }
+}
